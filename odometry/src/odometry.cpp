94ed99cd0c22eb95e7a2f3b7cec693af8fcec87b
robert server <robertglcasa@gmail.com>
2017-02-01 14:30:49 +0100

    ex1 completed

--- odometry/src/odometry.cpp
+++ odometry/src/odometry.cpp
@@ -9,6 +9,8 @@
 #include <unistd.h>
 #include <sstream>
 
+using namespace std;
+
 class Odometry {
 public:
   Odometry(double _sigma_vx, double _sigma_vy, double _sigma_vth) {
@@ -44,6 +46,11 @@ public:
 
     //### Initial reset: factor pose increment's covariance ###
     C_D = Eigen::Matrix3d::Zero(3, 3);
+
+    vReset = 0;
+
+
+
   }
 
   void run() {
@@ -57,25 +64,64 @@ public:
       double delta_x  = 0;
       double delta_y  = 0;
       double delta_th = 0;
-      double Delta_x  = 0;
-      double Delta_y  = 0;
-      double Delta_th = 0;
-      C_D = Eigen::Matrix3d::Zero(3, 3);
-      double x  = 0;
-      double y  = 0;
-      double th = 0;
+//      double Delta_x  = 0;
+//      double Delta_y  = 0;
+//      double Delta_th = 0;
+//      C_D = Eigen::Matrix3d::Zero(3, 3);
+//      double x  = 0;
+//      double y  = 0;
+//      double th = 0;
       
+
+      Eigen::Matrix3d C_d, J_dvdt , J_D_D, J_D_d;
+
+      //### Set Jacobian of pose increment
+	  J_dvdt = Eigen::Matrix3d::Zero(3, 3);
+	  J_dvdt(0,0) = 1;
+	  J_dvdt(1,1) = 1;
+	  J_dvdt(2,2) = 1;
+
       //### Time Integration of Velocity Data ###
+
+      delta_x = vx * delta_t;
+      delta_y = vy * delta_t;
+      delta_th = vth  * delta_t;
      
       //### Time Integration of Velocity Data - Jacobian stage ###
-      
+      C_d = Eigen::Matrix3d::Zero(3, 3);
+      C_d = J_dvdt * C_v * J_dvdt.transpose() * delta_t;
+
       //### Integrate Factor ###
+
+      Delta_x = Delta_x + delta_x * cos(Delta_th) - delta_y * sin(Delta_th);
+      Delta_y = Delta_y + delta_x * sin (Delta_th) + delta_y * cos (Delta_th);
+      Delta_th = Delta_th + delta_th;
     
       //### Integrate Factor - Jacobian Stage ###
       
+      J_D_D = Eigen::Matrix3d::Zero(3, 3);
+      J_D_D(0,0) = 1;
+      J_D_D(1,1) = 1;
+      J_D_D(2,0) = -delta_x * sin(Delta_th) - delta_y * cos(Delta_th);
+      J_D_D(2,1) = delta_x * cos(Delta_th) - delta_y * sin(Delta_th);
+      J_D_D(2,2) = 1;
+
+      J_D_d = Eigen::Matrix3d::Zero(3, 3);
+      J_D_d(0,0) = cos(Delta_th);
+      J_D_d(0,1) = -sin(Delta_th);
+      J_D_d(1,0) = sin(Delta_th);
+      J_D_d(1,1) = cos(Delta_th);
+      J_D_d(2,2) = 1;
+
       //### Integrate Factor - Covariance stage ###
 
+      C_D = J_D_D * C_D * J_D_D.transpose() + J_D_d * C_d * J_D_d.transpose();
+
       //### Integrate Global Pose ###
+
+      x = x + delta_x * cos (th) - delta_y * sin (th);
+      y = y + delta_x * sin (th) + delta_y * cos (th);
+      th = th + delta_th;
 
       //### Publish d, D and global poses, and covariance C_D ###
       delta_pub.publish(create_pose_msg(delta_x, delta_y, delta_th));
@@ -83,26 +129,35 @@ public:
       C_D_pub.publish(create_covariance_msg(C_D));
       global_pub.publish(create_pose_msg(x, y, th));
 
+      if(vReset < 30)	vReset++;
+      else{
+    	  Delta_x = 0;
+    	  Delta_y = 0;
+    	  Delta_th = 0;
+    	  C_D = Eigen::Matrix3d::Zero(3, 3);
+    	  vReset = 0;
+      }
+
       last_time = current_time;
       ros::spinOnce();
     }
   }
 
   void keyframe_callback() {
-    //### Publish factor data
-    // JS: publish Delta_x, Delta_y, Delta_th
-    // DS: Delta_pub.publish(create_pose_msg(Delta_x, Delta_y, Delta_th));
-    // JS: publish C_D;
-    // DS: C_D_pub.publish(create_covariance_msg(C_D));
+     //### Publish factor data
+     // JS: publish Delta_x, Delta_y, Delta_th
+     // DS: Delta_pub.publish(create_pose_msg(Delta_x, Delta_y, Delta_th));
+     // JS: publish C_D;
+     // DS: C_D_pub.publish(create_covariance_msg(C_D));
 
-    //### Keyframe reset: factor pose increment ###
-    //Delta_x  = 0.0;
-    //Delta_y  = 0.0;
-    //Delta_th = 0.0;
+     //### Keyframe reset: factor pose increment ###
+     //Delta_x  = 0.0;
+     //Delta_y  = 0.0;
+     //Delta_th = 0.0;
 
-    //### Keyframe reset: factor pose increment's covariance ###
-    //C_D = Eigen::Matrix3d::Zero(3, 3);
-  }
+     //### Keyframe reset: factor pose increment's covariance ###
+     //C_D = Eigen::Matrix3d::Zero(3, 3);
+   }
 
 private:
   ros::NodeHandle n;
@@ -113,6 +168,8 @@ private:
   double x, y, th;
   double vx, vy, vth;
   double Delta_x, Delta_y, Delta_th;
+
+  int vReset;
 
   geometry_msgs::Pose create_pose_msg(double x, double y, double th) {
     geometry_msgs::Quaternion pose_quat = tf::createQuaternionMsgFromYaw(th);
